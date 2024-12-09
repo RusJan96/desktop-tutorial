2b8d85dec484fd290ce12aed4e30cf16ebbfb42c
RusJan96 <Khasanova109@mail.ru>
2024-12-09 20:35:21 +0700

    сделал реквест
    сделал домашку реквест мпнеджер

--- home_work/request_manager2/requestmanager/requestmanager/settings.py
+++ home_work/request_manager2/requestmanager/requestmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zxx2p2ks(6&1u!(hfa8cwpe9k1vrom5c+pi_ihvk8djihs$5^^'
+SECRET_KEY = 'django-insecure-sdl(9r$yc8p!ae#4c4w(mcpa0$1!csct%u9@*2n1nbmcj3z9km'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'requests'
+    'requests',
 ]
 
 MIDDLEWARE = [
--- home_work/request_manager2/requestmanager/requests/views.py
+++ home_work/request_manager2/requestmanager/requests/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from .forms import RequestForm
 from .models import Request
 
-
 def create_request(request):
     if request.method == 'POST':
         form = RequestForm(request.POST)
@@ -11,10 +10,9 @@ def create_request(request):
             return redirect('request_list')
     else:
         form = RequestForm()
-    return render(request, 'requests/create_request.html', {'form':form}) 
+    return render(request, 'requests/create_request.html', {'form': form})
 
 def request_list(request):
-    requests = Request.object.all()
-    return render(request, 'requests/request_list.html', {'requests':requests})       
+    requests = Request.objects.all()
+    return render(request, 'requests/request_list.html', {'requests': requests})
 
-# Create your views here.
